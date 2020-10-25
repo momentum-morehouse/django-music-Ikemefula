46fd27a94a6496fae55039ccfaf5665bcc92b3bf
Ikemefula Orisakwe <IkeOrisakwe@gmail.com>
2020-10-25 18:50:04 -0500

    changes

--- TraccTracc/models.py
+++ TraccTracc/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class TraccTracc(models.model):
+    title = models.CharField(max_length=100)
+    artist = models.CharField(max_length=100)
+    year_released = models.CharField(max_length=100)
--- django_music/settings.py
+++ django_music/settings.py
@@ -63,6 +63,7 @@ INSTALLED_APPS = [
 
     # Project-specific
     'users',
+    'TraccTracc',
 ]
 
 MIDDLEWARE = [
